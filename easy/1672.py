309023a9a649b2de46d840684ec5dc3b33ac0532
ICE0208 <doky3307@naver.com>
2022-04-07 12:00:58 +0900

    해결 완료

---
+++ easy/1672.py
@@ -0,0 +1,21 @@
+import math
+
+class Solution:
+    def maximumWealth(self, accounts):
+        money_list = []
+        for l in accounts:
+            money_list.append(sum(l))
+
+        return max(money_list)
+        
+
+
+
+
+# ==========================
+s = Solution()
+m = int(input('m : '))
+l = list()
+for i in range(m):
+    l.append(list(map(int, input().split(" "))))
+print(s.maximumWealth(l))
