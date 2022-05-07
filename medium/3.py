2b8b61cd67f862c0d5ad70fcead4b2b59a9c378c
ICE0208 <doky3307@naver.com>
2022-05-07 13:52:06 +0900

    해결 완료 / discuss

--- medium/3.py
+++ medium/3.py
@@ -2,24 +2,33 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        s_list = list(s)
-        temp_list = []
-        cnt = 0
-        max_cnt = 0
-        for i in range(len(s_list)):
-            for c in s_list[i:]:
-                if c not in temp_list:
-                    temp_list.append(c)
-                    cnt += 1
-                else:
-                    max_cnt = max(max_cnt, cnt)
-                    cnt = 0
-                    temp_list = []
-                    break
+        # ? New Code (2022-05-07)
+        char_count = len(set(s))
+        len_s = len(s)
+        for i in range(char_count, -1, -1):
+            for j in range(0, len_s - i + 1):
+                if len(set(s[j:j+i])) == i:
+                    return i
 
-            max_cnt = max(max_cnt, cnt)
+        # ? Old Code (2022-04-07)
+        # s_list = list(s)
+        # temp_list = []
+        # cnt = 0
+        # max_cnt = 0
+        # for i in range(len(s_list)):
+        #     for c in s_list[i:]:
+        #         if c not in temp_list:
+        #             temp_list.append(c)
+        #             cnt += 1
+        #         else:
+        #             max_cnt = max(max_cnt, cnt)
+        #             cnt = 0
+        #             temp_list = []
+        #             break
 
-        return max_cnt
+        #     max_cnt = max(max_cnt, cnt)
+
+        # return max_cnt
 
 # =============
 s = Solution()
