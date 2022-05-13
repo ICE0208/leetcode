502b98c4e675c9786d8506db5f47e40adb21ae59
ICE0208 <doky3307@naver.com>
2022-05-13 23:28:36 +0900

    해결 완료 (2)

--- easy/70.py
+++ easy/70.py
@@ -1,20 +1,36 @@
 # https://leetcode.com/problems/climbing-stairs/
 
+# 2차 풀이
 class Solution:
-    def climbStairs(self, n):
-        l = n
-        r = 0
-        now = 1
-        sum = 1
-        while l>r:
-            print(f'{l-1}C{r+1} = ',end=' ')
-            now = (now * ((l-r)*(l-r-1) / ((r+1)*l)))
-            print((now))
-            l -= 1
-            r += 1
-            sum += now
-        print(sum)
-        return round(sum)
+    def climbStairs(self, n: int) -> int:
+    
+        fn_n = 1
+        fn_1 = 1
+        fn_2 = 1
+        
+        for i in range(n-2, -1, -1):
+            fn_n = fn_1 + fn_2
+            fn_2 = fn_1
+            fn_1 = fn_n
+
+        return fn_n
+
+# 1차 풀이
+# class Solution:
+#     def climbStairs(self, n):
+#         l = n
+#         r = 0
+#         now = 1
+#         sum = 1
+#         while l>r:
+#             print(f'{l-1}C{r+1} = ',end=' ')
+#             now = (now * ((l-r)*(l-r-1) / ((r+1)*l)))
+#             print((now))
+#             l -= 1
+#             r += 1
+#             sum += now
+#         print(sum)
+#         return round(sum)
 
 # ======
 n = int(input('n: '))
