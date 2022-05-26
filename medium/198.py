2d51d7b2250bee4bbec5e70d0635f0b2adce8150
ICE0208 <doky3307@naver.com>
2022-05-26 13:18:03 +0900

    해결 완료 (2)

--- medium/198.py
+++ medium/198.py
@@ -1,6 +1,16 @@
 # https://leetcode.com/problems/house-robber/
 
+# 2차 풀이
 class Solution:
+    def rob(self, nums):
+        cash = {}
+        
+        for i in range(len(nums)-1, -1, -1):
+            cash[i] = max(cash.get(i+2, 0), cash.get(i+3, 0)) + nums[i]
+        return max(cash.get(0, 0), cash.get(1, 0))
+
+# 1차 풀이
+class OldSolution:
     def rob(self, nums):
         if len(nums) < 3:
             return max(nums)
