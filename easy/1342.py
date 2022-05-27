f9b1249a2d3e528d44d5474ba341a86ac545d6da
ICE0208 <doky3307@naver.com>
2022-05-27 22:01:19 +0900

    해결 완료 (2)

--- easy/1342.py
+++ easy/1342.py
@@ -1,6 +1,17 @@
 # https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/submissions/
 
 class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        cnt = 0
+        while num:
+            if num%2:
+                num-=1
+            else:
+                num//=2
+            cnt += 1
+        return cnt
+
+class OldSolution:
     def numberOfSteps(self, num: int):
         cnt = 0
         while num:
