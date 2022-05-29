e8f6c167cc91e6783f08fcf56745ce4f68f7a2be
ICE0208 <doky3307@naver.com>
2022-05-29 13:56:30 +0900

    해결 완료 / 검토 필요

--- medium/318.py
+++ medium/318.py
@@ -2,9 +2,19 @@
 class Solution:
     def maxProduct(self, words):
         res = 0
+        max_j = 0
+        words.sort(key=lambda x: len(x), reverse=True)
         for i in range(len(words)-1):
+            len_i= len(words[i])
+            if res > len_i * len(words[i+1]):
+                return res
             for j in range(i+1, len(words)):
+                len_j = len(words[j])
+                if len_j <= max_j:
+                    break
                 if set(words[i]) & set(words[j]):
                     continue
-                res = max(res, len(words[i])*len(words[j]))
+                if len_i*len_j > res:
+                    res = len_i*len_j
+                    max_j = len_j
         return res
