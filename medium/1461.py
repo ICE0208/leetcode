5fc917a733d52bbba495665aaab54092564dbe05
ICE0208 <doky3307@naver.com>
2022-05-31 14:03:52 +0900

    해결 완료 (2) / 검토 필요

--- medium/1461.py
+++ medium/1461.py
@@ -1,8 +1,22 @@
 # https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
 
-# 1차 풀이
+# 2차 풀이
 class Solution:
-    def hasAllCodes(self, s: str, k: int) -> bool:
+    def hasAllCodes(self, s: str, k: int):
+        hashset = set(range(0, 2**k))
+            
+        for i in range(0, len(s)-k+1):
+            target = int(s[i:i+k], 2)
+            if target in hashset:
+                hashset.remove(target)
+        
+        if hashset:
+            return False
+        return True
+
+# 1차 풀이
+class OldSolution:
+    def hasAllCodes(self, s: str, k: int):
         hashset = set()
         for n in range(0, 2**k):
             hashset.add(f"{n:b}".zfill(k))
