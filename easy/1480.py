33ab9a60c4b7f493f4c6b83a286d2e14fe336209
ICE0208 <doky3307@naver.com>
2022-06-01 20:56:53 +0900

    해결 완료 (2)

--- easy/1480.py
+++ easy/1480.py
@@ -1,6 +1,14 @@
 # ahttps://leetcode.com/problems/running-sum-of-1d-array/
 
+# 2차 풀이
 class Solution:
+    def runningSum(self, nums):
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+
+# 1차 풀이
+class OldSolution:
     def runningSum(self, nums):
         sum_ary = []
         sum = 0
